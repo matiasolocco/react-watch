6eb04b2e2547f62c75fc7727e56a7d4f206dca37
matiasolocco <matias.olocco@hotmail.com>
2024-05-12 12:02:21 +0200

    React Watch Class Project

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+
+//todos los componentes comienzan en mayusuclas
+function App() {
+  //todo lo que quiero que se vea en la web va en el return
+  return (
+  <div>
+    <div>
+      <h1> Template </h1>
+      ;
+    </div>
+  </div>
+  )
+    
+  
+}
+//se exporta como todos los componentes y luego se importa
+export default App
